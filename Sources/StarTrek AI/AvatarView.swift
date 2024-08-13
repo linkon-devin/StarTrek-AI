54b20d22aa9da1e536a4ecad2b26341025d228b5
Linkon <linkon@Linkons-MacBook-Pro.local>
2024-08-13 21:48:44 +0600

    first commit

---
+++ Sources/StarTrek AI/AvatarView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Linkon on 13/8/24.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    var body: some View {
+        Text("Hello, World!")
+                    .font(.title)
+                    .padding()
+        Button(action: {
+            print("")
+        }, label: {
+            Text("asdasd")
+        })
+    }
+}
+
+#Preview {
+    AvatarView()
+}
